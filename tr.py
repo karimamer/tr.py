5081e926ac78b3c162054ee0d5549a22f6926c49
karimamer <karim.amer.sa@gmail.com>
2015-06-15 16:57:42 -0700

    push

---
+++ tr.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+import subprocess
+f = open("oscar.txt")
+trcomand ="tr -sc 'A-Za-z' '\n' < oscar.txt | sort | uniq -c | sort -n -r | tr -sc 'A-Za-z' | sort -n -r "
+runcd = subprocess.check_output(trcomand, shell = True)
+try:
+	for line in runcd.split('\n'):
+		count = line.split(" ")[1]
+		word = line.split(" ")[2]
+		x =(count,word)
+		print x 
+       
+         
+
+except:
+	print "empty line" 	
+
+
+
+
+
+
+
